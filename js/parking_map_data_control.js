f722f6870fea89b069aaf649da9e6a17e74cc89c
Mark Brown <markabrown87@gmail.com>
2018-09-08 18:36:01 -0400

    fix-legend-css Replaced table html structure for legend with responsive flexbox

--- js/parking_map_data_control.js
+++ js/parking_map_data_control.js
@@ -9,21 +9,40 @@ L.Control.ParkingInput = L.Control.extend({
     const container = L.DomUtil.create('div', 'container')
 
     const inputPanel = L.DomUtil.create('div', 'panel panel-default', container)
-    inputPanel.innerHTML = '<div class="row align-items-end">' +
-      '<div class="col-xs-12"><font size="+6">Wilmington, DE Parking</font></div>' +
-      '<div class="col-xs-2">Metered Parking</div>' +
-      '<div class="col-xs-10"><input type="checkbox" id="hasMeters">&nbsp;</input></div>' +
-      '<div class="col-xs-2">Accepts Credit Cards</div>' +
-      '<div class="col-xs-10"><input type="checkbox" id="acceptsCreditCards">&nbsp;</input></div>' +
-      '<div class="col-xs-2">Accepts Park Mobile</div>' +
-      '<div class="col-xs-10"><input type="checkbox" id="acceptsParkMobile">&nbsp;</input></div>' +
-      '<div class="col-xs-2">Accepts Coins</div>' +
-      '<div class="col-xs-10"><input type="checkbox" id="acceptsCoins">&nbsp;</input></div>' +
-      '<div class="col-xs-2">Free Saturday Parking</div>' +
-      '<div class="col-xs-10"><input type="checkbox" id="freeSaturdayParking">&nbsp;</input></div>' +
-      '<div class="col-xs-2">Free Sunday Parking</div>' +
-      '<div class="col-xs-10"><input type="checkbox" id="freeSundayParking">&nbsp;</input></div>' +
-      '</div>'
+    inputPanel.innerHTML = 
+      '<h3>Wilmington, DE</h3>' +
+      '<form id="filter-parking-form">' +
+        '<div>' +
+        '<label>' +
+          '<input type="checkbox" id="hasMeters" /> Metered Parking' +
+        '</label>' +
+        '</div>' +
+        '<div>' +
+        '<label>' +
+          '<input type="checkbox" id="acceptsCreditCards" /> Accepts Credit Cards' +
+        '</label>' +
+        '</div>' +
+        '<div>' +
+        '<label>' +
+          '<input type="checkbox" id="acceptsParkMobile" /> Accepts Park Mobile' +
+        '</label>' +
+        '</div>' +
+        '<div>' +
+        '<label>' +
+          '<input type="checkbox" id="acceptsCoins" /> Accepts Coins' +
+        '</label>' +
+        '</div>' +
+        '<div>' +
+        '<label>' +
+          '<input type="checkbox" id="freeSaturdayParking" /> Free Saturday Parking' +
+        '</label>' +
+        '</div>' +
+        '<div>' +
+        '<label>' +
+          '<input type="checkbox" id="freeSundayParking" /> Free Sunday Parking' +
+        '</label>' +
+        '</div>' +
+      '</form>'
 
     return container
   },
